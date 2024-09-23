2e0dc545aecd8ee3f3ca63c6c9baf412aa488476
Ali BARIN <ali.barin53@gmail.com>
2024-09-23 13:42:19 +0000

    test(SamlAuthProvidersRoleMapping): remove test case for relation mappings

--- packages/backend/src/models/saml-auth-providers-role-mapping.ee.test.js
+++ packages/backend/src/models/saml-auth-providers-role-mapping.ee.test.js
@@ -1,6 +1,5 @@
 import { describe, it, expect } from 'vitest';
 import SamlAuthProvidersRoleMapping from '../models/saml-auth-providers-role-mapping.ee';
-import SamlAuthProvider from './saml-auth-provider.ee';
 
 describe('SamlAuthProvidersRoleMapping model', () => {
   it('tableName should return correct name', () => {
@@ -19,16 +18,5 @@ describe('SamlAuthProvidersRoleMapping model', () => {
     expect(SamlAuthProvidersRoleMapping).toRequireProperty(
       'samlAuthProviderId'
     );
-  });
-
-  it('relationMappings should have samlAuthProvider relation', () => {
-    const samlAuthProviderRelation =
-      SamlAuthProvidersRoleMapping.relationMappings().samlAuthProvider;
-
-    expect(samlAuthProviderRelation).toMatchSnapshot();
-    expect(samlAuthProviderRelation.relation).toStrictEqual(
-      SamlAuthProvidersRoleMapping.BelongsToOneRelation
-    );
-    expect(samlAuthProviderRelation.modelClass).toStrictEqual(SamlAuthProvider);
   });
 });
