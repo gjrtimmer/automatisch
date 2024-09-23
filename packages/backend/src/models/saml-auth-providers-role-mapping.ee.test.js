7b17a61c323c87336c3bcd15353a7b1dbf3cf650
Ali BARIN <ali.barin53@gmail.com>
2024-09-23 12:01:02 +0000

    test(SamlAuthProvidersRoleMapping): assert model properties

---
+++ packages/backend/src/models/saml-auth-providers-role-mapping.ee.test.js
@@ -0,0 +1,31 @@
+import { describe, it, expect } from 'vitest';
+import SamlAuthProvidersRoleMapping from '../models/saml-auth-providers-role-mapping.ee';
+import assertRequiredSchemaProperties from '../../test/helpers/assert-required-model-properties';
+import SamlAuthProvider from './saml-auth-provider.ee';
+
+describe('SamlAuthProvidersRoleMapping model', () => {
+  it('tableName should return correct name', () => {
+    expect(SamlAuthProvidersRoleMapping.tableName).toBe(
+      'saml_auth_providers_role_mappings'
+    );
+  });
+
+  it('jsonSchema should have the correct schema', () => {
+    expect(SamlAuthProvidersRoleMapping.jsonSchema).toMatchSnapshot();
+  });
+
+  it('required properties should be required', async () => {
+    await assertRequiredSchemaProperties(SamlAuthProvidersRoleMapping);
+  });
+
+  it('relationMappings should have samlAuthProvider relation', () => {
+    const samlAuthProviderRelation =
+      SamlAuthProvidersRoleMapping.relationMappings().samlAuthProvider;
+
+    expect(samlAuthProviderRelation).toMatchSnapshot();
+    expect(samlAuthProviderRelation.relation).toStrictEqual(
+      SamlAuthProvidersRoleMapping.BelongsToOneRelation
+    );
+    expect(samlAuthProviderRelation.modelClass).toStrictEqual(SamlAuthProvider);
+  });
+});
